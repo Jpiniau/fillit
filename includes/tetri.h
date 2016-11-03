ae800c7895ed83593dc31afcc7d4fec814d0ca94
Jean-baptiste PINIAU <jpiniau@e3r4p12.42.fr>
2016-11-03 13:07:21 +0100

    includes

--- includes/tetri.h
+++ includes/tetri.h
@@ -6,7 +6,7 @@
 /*   By: jpiniau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/27 12:49:07 by jpiniau           #+#    #+#             */
-/*   Updated: 2016/10/31 15:59:39 by mdeken           ###   ########.fr       */
+/*   Updated: 2016/11/03 11:36:46 by jpiniau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,7 @@ void	free_tetri(char **tetri);
 t_list	*get_all_tetri(char *path);
 char	**get_tetri(int fd);
 int		test_tetri(char ***tetri);
+char	**go_topleft(char **tetri);
+void	print_tetri(char **tetri);
 
 #endif
